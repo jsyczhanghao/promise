ab707a521e09be56869c5fb045b459c16edff035
zhanghao <jsyczhanghao@sina.com>
2018-08-30 21:36:36 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+//var Promise = require('./lib');
+
+var p = new Promise((resolve, reject) => {
+    resolve(33);
+});
+
+var d;
+
+var b = p.then((data) => {
+    return d = new Promise((resolve, reject) => {
+        console.log('3333')
+        resolve(344);
+    }).then((data) => {
+        console.log(data);
+        return new Promise((resolve, reject) => {
+            reject('3333');
+        }).then(() => {}, () => {
+            console.log('reject');
+            return 333;
+        })
+    });
+});
+
+var c = b.then((data) => {
+    console.log(data, 333);
+}, function(){
+    console.log('1233reject');
+    return 123;
+});
+
+c.then((data) => {
+    console.log(data, 'lalal')
+}).then((data) => {
+    console.log('fdjkfdjsk');
+})
+
+// setTimeout(() => {
+//     console.log(b, c, b === c, b === d);
+// }, 100)
+
+Promise.all([
+    new Promise(function(resolve){
+        console.log(1);
+        resolve(1);
+    }),
+
+    (new Promise(function(resolve, reject){
+        console.log(2);
+        reject(2);
+    })).then(() => {
+        return new Promise(resolve => {
+            console.log(3);
+            resolve(3);
+        })
+    }, () => {
+        return 3;
+    })
+]).then((args) => {
+    console.log(4, args);
+}, () => {
+    console.log(5);
+})
---
+++ lib.js
@@ -0,0 +1,115 @@
+var STATES = {
+    PENDING: 1,
+    RESOLVED: 2,
+    REJECTED: 3
+};
+
+function nextTick(callback){
+    setTimeout(callback, 0);
+}
+
+function b(data){return data};
+
+function Promise(fn){
+    this.state = STATES.PENDING;
+    this._ = [];
+    this._finally = b;
+    this._ = [];
+    fn(this.emitResolve.bind(this), this.emitReject.bind(this));
+}
+
+Promise.prototype = {
+    then: function(resolve, reject){
+        var promise = new Promise(function(){});
+
+        this._.push({
+            resolve: resolve,
+            reject: reject,
+            promise: promise
+        });
+
+        return promise;
+    },
+
+    emitResolve: function(data){
+        var self = this;
+
+        nextTick(function(){
+            self.state = STATES.RESOLVED;
+            self.emitFinally(data);
+        });
+    },  
+
+    emitReject: function(data){
+        var self = this;
+
+        nextTick(function(){
+            self.state = STATES.REJECTED;
+            self.emitFinally(data);
+        });
+    },
+
+    emitFinally(data){
+        var isResolved = this.state == STATES.RESOLVED;
+
+        this._.map(function(info){
+            if(isResolved){
+                res = info.resolve(data);
+            }else{
+                if(info.reject){
+                    res = info.reject(data);
+                }else{
+                    info.promise.emitReject();
+                    return;
+                }
+            } 
+
+            if(!(res instanceof Promise)){
+                info.promise.emitResolve(res);
+            }else{
+                res.then(function(data){
+                    info.promise.emitResolve(data);
+                }, function(data){
+                    info.promise.emitReject(data);
+                });
+            }
+        });
+
+        this._.length = 0;
+    }
+};
+
+Promise.all = function(promises){
+    var allPromise = new Promise(function(resolve, reject){
+        var datas = [], l = promises.length;
+
+        promises.map(function(promise){
+            promise.then(function(data){
+                if(allPromise.state != STATES.PENDING){
+                    return false;
+                }
+
+                datas.push(data);
+                datas.length == l && resolve(datas);
+            }, function(data){
+                reject(data);
+            });
+        });
+    });
+
+    return allPromise;
+};
+
+Promise.resolve = function(data){
+    return new Promise(function(resolve){
+        resolve(data);
+    });
+};
+
+Promise.reject = function(data){
+    return new Promise(function(resolve, reject){
+        reject(data);
+    });
+}
+
+module.exports = Promise;
